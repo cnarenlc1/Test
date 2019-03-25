d62c6802f675ebe724e2a9973354ba49dfcca448
Madhuv2 <lvutukury@gmail.com>
2019-03-24 20:47:55 -0500

    new method added

--- src/MainApp.java
+++ src/MainApp.java
@@ -3,4 +3,8 @@ public class MainApp {
     public static void main(String[] args) {
         System.out.println("This is the test");
     }
+
+    public static  int add(int a, int b){
+        return (a-b);
+    }
 }
